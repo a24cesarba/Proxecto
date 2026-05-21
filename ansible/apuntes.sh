4aae57025aaeadd9b0d0cda2949f7da75ef965ee
a24cesarba <a24cesarba@iessanclemente.net>
2026-05-21 23:22:01 +0200

    Swarm listo

--- ansible/apuntes.sh
+++ ansible/apuntes.sh
@@ -1,14 +1,2 @@
-<<<<<<< HEAD
-#script que debe:
-# Crear pareja de claves
-# Copiar clave pública en las máquinas
-# Ejecutar playbooks
-ssh-copy-id ansible@172.16.30.20
-ssh-copy-id ansible@172.16.30.21
-ssh-copy-id ansible@172.16.30.22
-ssh-copy-id ansible@172.16.30.23
-ssh-copy-id ansible@172.16.30.24
-=======
 #/bin/bash
 vagrant ssh-config > ./ssh_config
->>>>>>> 612a826 (la experiencia)
